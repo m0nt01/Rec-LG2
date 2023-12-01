fc36d72907454edccc60918a6b23d70f4690967a
Powergu1d0 <kalwardin739@gmail.com>
2023-12-01 11:37:27 -0300

    Tudo pronto

--- .idea/src/TesteBuscaSequencial.java
+++ .idea/src/TesteBuscaSequencial.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
 
-public class TesteBuscaBinaria {
+public class TesteBuscaSequencial {
     public static void main(String[] args) {
         Random rand = new Random();
         Integer[] vetor = new Integer[10000];
@@ -13,15 +13,15 @@ public class TesteBuscaBinaria {
             vetor[i] = rand.nextInt(100000);
         }
 
-        BuscaBinaria<Integer> buscaBinaria = new BuscaBinaria<>();
+        BuscaSequencial<Integer> buscaSequencial = new BuscaSequencial<>();
 
         // Criar arquivo para gravar os resultados
-        try (PrintWriter writer = new PrintWriter(new FileWriter("resultados_busca_binaria.txt"))) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("resultados_busca_sequencial.txt"))) {
             // Buscar aleatoriamente 10% dos elementos
             for (int i = 0; i < vetor.length / 10; i++) {
                 int chave = vetor[rand.nextInt(vetor.length)];
                 long startTime = System.nanoTime();
-                int resultado = buscaBinaria.buscaBinaria(vetor, chave);
+                int resultado = buscaSequencial.buscaSequencial(vetor, chave);
                 long endTime = System.nanoTime();
                 double tempoEmSegundos = (endTime - startTime) / 1e9;
 
